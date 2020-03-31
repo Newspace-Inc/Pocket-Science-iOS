f8221e5e61d69570b53fe8c825892ee33c64f631
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2020-03-31 21:39:42 +0800

    Initial Commit

---
+++ Pocket Science/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pocket Science
+//
+//  Created by Ethan Chew on 31/3/20.
+//  Copyright © 2020 Ethan Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
