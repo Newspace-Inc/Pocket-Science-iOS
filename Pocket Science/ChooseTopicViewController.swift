b8d1abffcc8ed0ce05433c221456f7f7b11ed74f
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2020-06-20 23:20:35 +0800

    Bug Fixes

--- Pocket Science/ChooseTopicViewController.swift
+++ Pocket Science/ChooseTopicViewController.swift
@@ -43,10 +43,32 @@ class ChooseTopicViewController: UIViewController {
             userDefaults.set(primaryLevel, forKey: "Recently Opened")
         }
         
-        // Get Data from Data File (Data.xlsx)
-//        let filepath = "./Data.xlsx"
-//        guard let file = XLSXFile(filepath: filepath) else {
-//          fatalError("XLSX file at \(filepath) is corrupted or does not exist")
+        // Collect Data
+//        do {
+//            guard let filepath = Bundle.main.path(forResource: "data", ofType: "xlsx") else {
+//                return
+//            }
+//            guard let file = XLSXFile(filepath: filepath) else {
+//              fatalError("XLSX file at \(filepath) is corrupted or does not exist")
+//            }
+//
+//            for wbk in try file.parseWorkbooks() {
+//              for (name, path) in try file.parseWorksheetPathsAndNames(workbook: wbk) {
+//                if let worksheetName = name {
+//                  print("This worksheet has a name: \(worksheetName)")
+//                    
+//                }
+//
+//                let worksheet = try file.parseWorksheet(at: path)
+//                for row in worksheet.data?.rows ?? [] {
+//                  for c in row.cells {
+//                    print(c)
+//                  }
+//                }
+//              }
+//            }
+//        } catch {
+//            fatalError("An error occured. \(error.localizedDescription)")
 //        }
         
     }
