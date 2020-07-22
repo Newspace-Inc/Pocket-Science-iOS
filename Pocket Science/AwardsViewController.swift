56566baa64cecc6e79d8c974f5d3f943f1613b3e
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2020-07-22 20:15:41 +0800

    Revert "Update"
    This reverts commit f892fcec0bc6604f2c4b230fd91f492ac59bff57.

--- Pocket Science/AwardsViewController.swift
+++ Pocket Science/AwardsViewController.swift
@@ -58,8 +58,8 @@ class AwardsViewController: UIViewController {
         uiBG.clipsToBounds = true
         
         // Set Corner Radius
-        badgesLabel.layer.cornerRadius = 20
-        rankLabel.layer.cornerRadius = 20
+        badgesLabel.layer.cornerRadius = 30
+        rankLabel.layer.cornerRadius = 30
         userRankLabel.layer.cornerRadius = 20
         uiBG.layer.cornerRadius = 20
     }
