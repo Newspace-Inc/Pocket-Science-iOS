9d288cbb0e68e54b7f63c7ad014798f9f0c77063
Ethan Chew <ethanchewmh@gmail.com>
2021-09-14 10:36:26 +0800

    Structuring of Files

---
+++ PocketScience/PostHome/ThemeSelectorView.swift
@@ -0,0 +1,22 @@
+//
+//  ThemeSelectorView.swift
+//  PocketScience
+//
+//  Created by Ethan Chew on 14/9/21.
+//
+
+import SwiftUI
+
+struct ThemeSelectorView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+        }
+    }
+}
+
+struct ThemeSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeSelectorView()
+    }
+}
